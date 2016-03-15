0b9d3dd61c577d1b2bbcb215e921dbae94b9558f
ty-l <996651583@qq.com>
2016-03-15 14:35:43 +0800

    感觉风格是C风格，要改

---
+++ test_passward.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test_passward.cpp
+	> Author: ty-l
+	> Mail: liuty196888@gmail.com 
+	> Created Time: 2016年03月15日 星期二 11时16分30秒
+ ************************************************************************/
+
+#include<iostream>
+#include"passward.cpp"
+
+int main(){
+	lty::userface();
+}
